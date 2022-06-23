d73199dc62417d787a5608de05dd7a3a5b480fd7
mzubair31102 <mzubair31102@gmail.com>
2022-06-23 11:49:25 +0500

    Pdf Data Extraction Using Python 1.1

--- Pdf-Data-Extraction-Using-Python.py
+++ Pdf-Data-Extraction-Using-Python.py
@@ -1,1 +1,17 @@
-print("Hi Muhammad Zubair is Here!")
+print("")
+
+from PyPDF2 import PdfReader
+# Regular Expression to Filter Data
+import re
+reader = PdfReader("CV-demo-1.pdf")
+number_of_pages = len(reader.pages)
+page_count = reader.getNumPages()
+page = reader.pages[0]
+text = page.extract_text()
+text = text.replace('\n','')
+print(text)
+# Email Extraction
+email = re.search(r'([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)',text).group()
+print(email)
+phone_num = re.search(r'(?:\+?\d{2}[ -]?)?\d{10}',text).group()
+print(phone_num)
