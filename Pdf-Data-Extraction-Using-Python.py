a1a57465a8e867a2087f5a0a8903f70591544738
mzubair31102 <mzubair31102@gmail.com>
2022-06-23 20:09:08 +0500

    Email, Number, First Name, Last Name, Skills Filtered

--- Pdf-Data-Extraction-Using-Python.py
+++ Pdf-Data-Extraction-Using-Python.py
@@ -1,9 +1,8 @@
-print("")
-
+print("---------CV Data Extraction---------")
 from PyPDF2 import PdfReader
 # Regular Expression to Filter Data
 import re
-reader = PdfReader("CV-demo-2.pdf")
+reader = PdfReader("CV-demo-1.pdf")
 number_of_pages = len(reader.pages)
 page_count = reader.getNumPages()
 page = reader.pages[0]
@@ -11,18 +10,8 @@ text = page.extract_text()
 text = text.replace('\n','')
 text = text.lower()
 print(text)
-# Email Extraction
-email = re.search(r'([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)',text).group()
-print(email)
-"""
-Expression for Mobile Formats 
-03024483861
-0302-4483861
-"""
-phone_num = re.search(r'(\d{4}[*-]?\d{7})',text).group()
-print(phone_num)
 # Name Extraction using multiple Expressions in single line
-
+print("---------First Name Extraction---------")
 f_name = ""
 if "name" in text:
     start_index = text.find("name")
@@ -30,7 +19,15 @@ if "name" in text:
         f_name+=text[i]
         if " " in  text[i]:
             break
-print(f_name)
+elif "dear" in text:
+    start_index = text.find("dear")
+    for i in range(start_index+6,start_index+200):
+        f_name+=text[i]
+        if " " in  text[i]:
+            break
+
+print(f_name.capitalize())
+print("---------Last Name Extraction---------")
 l_name= ""
 if f_name in text:
     start_index = text.find(f_name)
@@ -39,10 +36,28 @@ if f_name in text:
         l_name+=text[i]
         if " " in  text[i]:
             break
-print(l_name)
-skills = ""
-if "skills" in text:
-    print("YEs")
+print(l_name.capitalize())
+# Email Extraction
+print("---------Email Extraction---------")
+email = re.search(r'([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)',text).group()
+print(email)
+"""
+Expression for Mobile Formats 
+03024483861
+0302-4483861
+"""
+print("---------Phone Number Extraction---------")
+phone_num = re.search(r'(\d{4}[*-]?\d{7})',text).group()
+print(phone_num)
+print("---------Programming Skills Extraction---------")
+found_skills=""
+skills_list = ['html','css','bootsrap','javascript','java','php','python','dart',]
+for i in skills_list:
+    if i in text:
+        found_skills=found_skills+i+","
+print(found_skills)
+
+
 """
 get_f_name = re.compile("(?<=name: ).*?(?=\s)")
 print(get_f_name.findall(text))
